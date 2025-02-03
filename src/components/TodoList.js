1f3f163873fb336caa90c89b66054baa8be05ccb
axorious <makz-2001@mail.ru>
2025-02-03 17:40:54 +0300

    Server requests-task2 fix

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,7 +6,9 @@ const TodoList = () => {
 	const [todos, setTodos] = useState([]);
 	const [newTodo, setNewTodo] = useState('');
 	const [searchTerm, setSearchTerm] = useState('');
-	const [sort, setSort] = useState(false);
+	const [isSort, setIsSort] = useState(false);
+	const [editingTodoId, setEditingTodoId] = useState(null);
+	const [editingTodoText, setEditingTodoText] = useState('');
 
 	useEffect(() => {
 		fetchTodos();
@@ -17,11 +19,12 @@ const TodoList = () => {
 			const response = await axios.get('http://localhost:3005/todos');
 			setTodos(response.data);
 		} catch (error) {
-			console.error('ошибка при загрухзке данных', error);
+			console.error('ошибка при загрузке данных', error);
 		}
 	};
 
-	const addTodo = async () => {
+	const addTodo = async (e) => {
+		e.preventDefault();
 		if (newTodo.trim() === '') return;
 		try {
 			const response = await axios.post('http://localhost:3005/todos', {
@@ -31,18 +34,31 @@ const TodoList = () => {
 			setTodos([...todos, response.data]);
 			setNewTodo('');
 		} catch (error) {
-			console.error('ошибка при добавление дела:', error);
+			console.error('ошибка при добавлении дела:', error);
 		}
 	};
 
-	const updateTodo = async (id, completed) => {
+	const updateTodoStatus = async (id, completed) => {
 		try {
 			await axios.patch(`http://localhost:3005/todos/${id}`, {
 				completed: !completed,
 			});
 			fetchTodos();
 		} catch (error) {
-			console.error('ошибка при обновление дела:', error);
+			console.error('ошибка при обновлении статуса дела:', error);
+		}
+	};
+
+	const updateTodoText = async (id) => {
+		try {
+			await axios.patch(`http://localhost:3005/todos/${id}`, {
+				title: editingTodoText,
+			});
+			setEditingTodoId(null);
+			setEditingTodoText('');
+			fetchTodos();
+		} catch (error) {
+			console.error('ошибка при обновлении текста дела:', error);
 		}
 	};
 
@@ -51,7 +67,7 @@ const TodoList = () => {
 			await axios.delete(`http://localhost:3005/todos/${id}`);
 			setTodos(todos.filter((todo) => todo.id !== id));
 		} catch (error) {
-			console.log('ошибка при удаление дела:', error);
+			console.error('ошибка при удалении дела:', error);
 		}
 	};
 
@@ -59,22 +75,22 @@ const TodoList = () => {
 		todo.title.toLowerCase().includes(searchTerm.toLowerCase()),
 	);
 
-	const sortedTodos = sort
+	const sortedTodos = isSort
 		? [...filteredTodos].sort((a, b) => a.title.localeCompare(b.title))
 		: filteredTodos;
 
 	return (
 		<div className={styles.container}>
 			<h1 className={styles.title}>Todo List</h1>
-			<div className={styles.inputGroup}>
+			<form onSubmit={addTodo} className={styles.inputGroup}>
 				<input
 					type="text"
 					value={newTodo}
 					onChange={(e) => setNewTodo(e.target.value)}
 					placeholder="добавить новое дело"
 				/>
-				<button onClick={addTodo}>добавить</button>
-			</div>
+				<button type="submit">добавить</button>
+			</form>
 			<input
 				type="text"
 				value={searchTerm}
@@ -82,21 +98,50 @@ const TodoList = () => {
 				placeholder="поиск дел"
 				className={styles.searchInput}
 			/>
-			<button onClick={() => setSort(!sort)} className={styles.sortButton}>
-				{sort ? 'отменить сортировку' : 'сортировать по алфавиту'}
+			<button onClick={() => setIsSort(!isSort)} className={styles.sortButton}>
+				{isSort ? 'отменить сортировку' : 'сортировать по алфавиту'}
 			</button>
 			<ul className={styles.list}>
 				{sortedTodos.map((todo) => (
 					<li key={todo.id} className={styles.listItem}>
-						<span
-							className={
-								todo.completed ? styles.completed : styles.notCompleted
-							}
-							onClick={() => updateTodo(todo.id, todo.completed)}
-						>
-							{todo.title}
-						</span>
-						<button onClick={() => deleteTodo(todo.id)}>удалить</button>
+						{editingTodoId === todo.id ? (
+							<>
+								<input
+									type="text"
+									value={editingTodoText}
+									onChange={(e) => setEditingTodoText(e.target.value)}
+								/>
+								<button onClick={() => updateTodoText(todo.id)}>
+									сохранить
+								</button>
+								<button onClick={() => setEditingTodoId(null)}>
+									отмена
+								</button>
+							</>
+						) : (
+							<>
+								<span
+									className={
+										todo.completed
+											? styles.completed
+											: styles.notCompleted
+									}
+									onClick={() =>
+										updateTodoStatus(todo.id, todo.completed)
+									}
+								>
+									{todo.title}
+								</span>
+								<div>
+									<button onClick={() => setEditingTodoId(todo.id)}>
+										изменить
+									</button>
+									<button onClick={() => deleteTodo(todo.id)}>
+										удалить
+									</button>
+								</div>
+							</>
+						)}
 					</li>
 				))}
 			</ul>
